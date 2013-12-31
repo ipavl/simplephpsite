de8f36651f3cbd637dc9d328777bf4e215ca67fe
ipavl <>
2013-12-31 17:45:28 -0500

    Moved navigation links to their own file for easier locating

--- includes/header.php
+++ includes/header.php
@@ -24,17 +24,7 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse navbar-ex1-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.php?page=index">Home</a></li>
-                        <li><a href="index.php?page=broken">Sample broken link</a></li>
-                        <li><a href="index.php?page=sample">Sample page</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sample menu <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
-                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
-                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
-                            </ul>
-                        </li>
+                        <?php include('navigation.php'); ?>
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container -->
---
+++ includes/navigation.php
@@ -0,0 +1,12 @@
+                        <!-- Replace these with your navigation links -->
+                        <li><a href="index.php?page=index">Home</a></li>
+                        <li><a href="index.php?page=broken">Sample broken link</a></li>
+                        <li><a href="index.php?page=sample">Sample page</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sample menu <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
+                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
+                                <li><a href="index.php?page=subpage/sample">Sample drop-down link</a></li>
+                            </ul>
+                        </li>
