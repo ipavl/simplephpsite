442d37706b3209d709cd2c5c2eb801b0f3d4f9c4
ipavl <>
2013-12-30 22:37:03 -0500

    Removing ROBOTS information from index.php.

--- index.php
+++ index.php
@@ -34,7 +34,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW, NOARCHIVE, NOSNIPPET">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
