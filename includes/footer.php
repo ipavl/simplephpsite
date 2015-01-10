09104dcdfab38547780bc7366be525d13ae13bf4
ipavl <ipavl@users.sourceforge.net>
2015-01-10 12:03:55 -0500

    Update footer to show copyright info using config values

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,9 @@
 <!-- You can add to this, however it is requested that the following remain intact. -->
-<div id="footer-copyright">This website uses <a href="https://github.com/ipavl/simplephpsite" target="_blank">SimplePHPSite</a>, which includes <a href="http://www.fancyapps.com/fancybox/" target="_blank">Fancybox</a>.</div>
+<div id="footer-copyright">Copyright &copy; 2015
+  <?php
+    include('includes/config.php');
+    echo (isset($config['pageDir']) ? $config['titleText'] : "SimplePHPSite");
+  ?>.
+  <br/>Powered by <a href="https://github.com/ipavl/simplephpsite" target="_blank">SimplePHPSite</a>,
+  which includes <a href="http://www.fancyapps.com/fancybox/" target="_blank">Fancybox</a>.
+</div>
