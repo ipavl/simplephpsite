e896e3d982a60de9f583de482219490448cd140c
ipavl <ipavl@users.sourceforge.net>
2015-01-10 15:57:24 -0500

    Add site credits page

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,10 @@
-<!-- You can add to this, however it is requested that the following remain intact. -->
 <div id="footer-copyright">Copyright &copy; 2015
   <?php
     include('includes/config.php');
     echo (isset($config['pageDir']) ? $config['titleText'] : "SimplePHPSite");
   ?>.
-  <br/>Powered by <a href="https://github.com/ipavl/simplephpsite" target="_blank">SimplePHPSite</a>,
-  which includes <a href="http://www.fancyapps.com/fancybox/" target="_blank">Fancybox</a>.
+
+  <!-- You can add to this, however the following must remain intact. -->
+  <br/>Powered by <a href="https://github.com/ipavl/simplephpsite" target="_blank">SimplePHPSite</a>
+  and <a href="index.php?<?php echo $pageParam ?>=site-credits">other technologies</a>.
 </div>
