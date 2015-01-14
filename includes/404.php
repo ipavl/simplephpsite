a36f2397d2579255896caaeeb9b52df6a0d4349f
ipavl <ipavl@users.sourceforge.net>
2015-01-13 23:15:37 -0500

    Remove additional CSS classes from 404 page to keep it level with the sidebar

--- includes/404.php
+++ includes/404.php
@@ -1,12 +1,3 @@
-			<div class="row">
-				<div class="col-lg-12">
-					<h1 class="page-header">404 <small>Page Not Found</small></h1>
-				</div>
+<h1>404 <small>Page Not Found</small></h1>
 
-			</div>
-
-			<div class="row">
-				<div class="col-lg-12">
-				<p>Oops! The page you requested could not be found.</p>
-				</div>
-			</div>
+<p>Oops! The page you requested could not be found.</p>
