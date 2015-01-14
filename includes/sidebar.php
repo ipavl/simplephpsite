82ab1cdc369fc556939bb8878c2130e513de825c
ipavl <ipavl@users.sourceforge.net>
2015-01-13 23:10:49 -0500

    Add sidebar file

---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="col-lg-3">
+    <div class="sidebar">
+        <h3>Sidebar</h3>
+
+        <p>You can put links and other content (e.g. author bio) that should appear on every page here
+            by editing <code>includes/sidebar.php</code>.</p>
+
+        <p>Not your thing? Disable this sidebar by setting <code>sidebar</code> to <code>false</code>
+            in <code>includes/config.php</code>.</p>
+    </div>
+</div>
